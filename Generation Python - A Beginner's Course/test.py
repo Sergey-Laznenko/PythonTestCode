2495b2dd6d09d4908d125b465d5f32b7dd7fd834
Sergey <Laznenko.Sergey@gmail.com>
2020-08-23 21:13:23 +0300

    new

--- Generation Python - A Beginner's Course/test.py
+++ Generation Python - A Beginner's Course/test.py
@@ -1,5 +1,10 @@
 n = int(input())
+count = 0
+x = 1
+
 for i in range(1, n + 1):
-    for j in range(i):
-        print(i, end='')
-    print()
+    for j in range(1, i):
+        if i % j == 0:
+            count += 1
+        x += 1
+    print(i, '+' * count, sep='')
