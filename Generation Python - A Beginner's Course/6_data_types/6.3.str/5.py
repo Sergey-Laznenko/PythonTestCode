79baa55114d6ec74085446f01ea97b022980c277
Sergey <Laznenko.Sergey@gmail.com>
2020-08-26 11:19:59 +0300

    6.3_Task

---
+++ Generation Python - A Beginner's Course/6_data_types/6.3.str/5.py
@@ -0,0 +1,8 @@
+"""
+Напишите программу, которая выводит текст:
+"Python is a great language!", said Fred. "I don't ever remember having this much fun before."
+"""
+
+a = '"Python is a great language!", said Fred.'
+b = '"I don\'t ever remember having this much fun before."'
+print(a + ' ' + b)
---
+++ Generation Python - A Beginner's Course/6_data_types/6.3.str/6.py
@@ -0,0 +1,7 @@
+"""
+Напишите программу, которая считывает с клавиатуры две строки – имя и фамилию пользователя и выводит фразу:
+«Hello [введенное имя] [введенная фамилия]! You just delved into Python».
+"""
+
+a, b = input(), input()
+print(f'Hello {a} {b}! You just delved into Python')
