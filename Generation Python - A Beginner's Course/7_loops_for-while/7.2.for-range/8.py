fcf8b5a9f09ef06b4e3f647179c5f7d1b077c661
Sergey <Laznenko.Sergey@gmail.com>
2020-09-03 11:10:27 +0300

    11.6_Task

--- Generation Python - A Beginner's Course/7_loops_for-while/7.2.for-range/8.py
+++ Generation Python - A Beginner's Course/7_loops_for-while/7.2.for-range/8.py
@@ -10,3 +10,16 @@ if m < n:
 else:
     for i in range(m, n - 1, -1):
         print(i)
+
+count = 0
+p = 1
+for i in range(1, 11):
+    x = int(input())
+    if x >= 0:
+        p *= x
+        count += 1
+if count > 0:
+    print(count)
+    print(p)
+else:
+    print('NO')
