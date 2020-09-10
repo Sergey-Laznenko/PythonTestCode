8ed1c739fa5f4de4106794f1323204f776462093
Sergey <Laznenko.Sergey@gmail.com>
2020-09-10 14:51:34 +0300

    7.9_Task

--- Generation Python - A Beginner's Course/7_loops_for-while/7.9.nested loops_v2/7.py
+++ Generation Python - A Beginner's Course/7_loops_for-while/7.9.nested loops_v2/7.py
@@ -2,54 +2,15 @@
 На вход программе подается два натуральных числа a и b (a < b). Напишите программу,
 которая находит все простые числа от a до b включительно.
 """
-# Пока нет решения
-"""
+
+
 a, b = int(input()), int(input())
-lst = []
-k = 0
-for i in range(2, b + 1):
-    for j in range(2, i):
-        if i % j == 0:
-            k = k + 1
-    if k == 0:
-        lst.append(i)
-    else:
-        k = 0
-print(*lst, sep='\n')
-
-
-
-n = int(input())
-
-lst = []
-k = 0
-for i in range(2, n + 1):
-    for j in range(2, i):
-
-        if i % j == 0:
-            k = k + 1
-
-    if k == 0:
-        lst.append(i)
-    else:
-        k = 0
-
-print(lst)
-"""
-"""
-a, b = map(int, input().split())
-ls = []
 for i in range(a, b + 1):
-    if all(i % n != 0 for n in range(2, i)):
-        ls.append(str(i))
-if len(ls):
-    print(' '.join(ls))
-else:
-    print(0)
-"""
+    if i == 1:
+        continue
+    for j in range(2, (i // 2 + 1)):
+        if i % j == 0:
+            break
+    else:
+        print(i)
 
-a, b = int(input()), int(input())
-for i in range(a, b + 1,):
-    for j in range(1, i+1,):
-        if i > 1 and j == 1 or j == i:
-            print(i)
